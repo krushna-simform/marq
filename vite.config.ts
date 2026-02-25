20f72a60b425f92e2a8bce5f426aad82e5afa3ad
krushna rathod <krushna.rathod@simformsolutions.com>
2026-02-25 11:41:42 +0530

    chore: update package.json and vite.config.ts for library build and dependencies
    - Changed package name to "marq" and updated version to 0.0.1.
    - Added "files" and "publishConfig" sections in package.json.
    - Simplified build script in package.json.
    - Added Prettier format script in package.json.
    - Updated dependencies and added new devDependencies in package.json.
    - Enhanced vite.config.ts to include library build configuration.
    - Integrated vite-plugin-dts for TypeScript declaration file generation.
    - Added vite-plugin-lib-inject-css for CSS injection in library builds.
    - Configured Rollup options for library build in vite.config.ts.
    - Set up component entry points dynamically in vite.config.ts.
    - Added alias resolution for src directory in vite.config.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,45 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import { playwright } from "@vitest/browser-playwright";
+import fs from "fs";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
+
 const dirname =
   typeof __dirname !== "undefined"
     ? __dirname
     : path.dirname(fileURLToPath(import.meta.url));
 
+const srcDir = path.resolve(__dirname, "src/components");
+
+const entries: Record<string, string> = {};
+fs.readdirSync(srcDir).forEach((componentDir) => {
+  const componentDirPath = path.join(srcDir, componentDir);
+  const componentFilePath = path.join(componentDirPath, `${componentDir}.tsx`);
+
+  if (
+    fs.statSync(componentDirPath).isDirectory() &&
+    fs.existsSync(componentFilePath)
+  ) {
+    entries[`components/${componentDir}/${componentDir}`] = componentFilePath;
+  }
+});
+
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    libInjectCss(),
+    dts({
+      insertTypesEntry: false,
+      copyDtsFiles: true,
+      entryRoot: "src/components",
+      outDir: "dist/components",
+      tsconfigPath: "./tsconfig.app.json",
+      exclude: ["**/*.stories.tsx", "**/*.test.tsx", "**/__tests__/**"],
+    }),
+  ],
   test: {
     projects: [
       {
@@ -43,5 +74,28 @@ export default defineConfig({
         },
       },
     ],
+  },
+  build: {
+    lib: {
+      entry: entries,
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        preserveModules: true,
+        preserveModulesRoot: "src",
+        entryFileNames: "[name].js",
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
 });
